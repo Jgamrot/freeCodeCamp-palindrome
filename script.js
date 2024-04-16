4f50ab329fe9a4fe0538b24ea043a8d17c14d154
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-16 21:33:49 +0100

    Refactor to be somewhat functional
    Now doesn't pass all the tests, but
    actually does something with logic.
    
    Here I'm trying to turn the input string to
    a lowercase and trim it. Then compare it to the
    string  reversed.

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
-
 const result = document.getElementById("result");
 
+function checkIfPalindrome(str) {
+    const cleanedStr = str.toLowerCase().replace(/\s/g, "");
+    const reversedStr = str.split("").reverse().join("");
+    return cleanedStr === reversedStr;
+}
+
 checkBtn.addEventListener("click", () => {
-  
-    if (textInput.value.trim() === "") {
-        
-        alert("Please input a value");
-    } else if (textInput.value === "A"){
-        result.textContent = "A is a palindrome!"; 
-    } else if (textInput.value === "eye"){
-        result.textContent = "eye is a palindrome!"; 
-    } else if (textInput.value === "_eye"){
-        result.textContent = "_eye is a palindrome!"; 
-    } else if (textInput.value === "race car"){
-        result.textContent = "race car is a palindrome"; 
-    } else if (textInput.value === "not a palindrome"){
-        result.textContent = "not a palindrome is not a palindrome"; 
-    } else if (textInput.value === "A man, a plan, a canal. Panama"){
-        result.textContent = "A man, a plan, a canal. Panama is a palindrome"; 
-    }else if (textInput.value === "never odd or even"){
-        result.textContent = "never odd or even is a palindrome"; 
-    }else if (textInput.value === "nope"){
-        result.textContent = "nope is not a palindrome"; 
-    }else if (textInput.value === "almostomla"){
-        result.textContent = "almostomla is not a palindrome"; 
-    }else if (textInput.value === "My age is 0, 0 si ega ym."){
-        result.textContent = "My age is 0, 0 si ega ym. is a palindrome"; 
-    }else if (textInput.value === "1 eye for of 1 eye."){
-        result.textContent = "1 eye for of 1 eye. is not a palindrome"; 
-    }else if (textInput.value === "0_0 (: /-\ :) 0-0"){
-        result.textContent = "0_0 (: /-\ :) 0-0 is a palindrome"; 
-    }else if (textInput.value === "five|\_/|four"){
-        result.textContent = "five|\_/|four is not a palindrome"; 
+    const inputString = textInput.value.trim();
+
+      if (inputString === "") {
+            alert("Please input a value");
+    } else {
+        const isPalindrome = checkIfPalindrome(inputString);
+        result.textContent = isPalindrome
+        ? `${inputString} is a palindrome!`
+        : `${inputString} is not a palindrome!`;
     }
 });
-
